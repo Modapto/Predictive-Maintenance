3620a95b247429de539cb822794ee7b3eff19e2d
Mja1110 <truongle11102000@gmail.com>
2025-04-03 09:53:42 +0200

    update

---
+++ user_input/input.py
@@ -0,0 +1,4 @@
+# User input
+setup_cost = 500                           # Setup cost
+downtime_cost_rate = 100                           # Downtime cost rate
+no_repairmen = 1                               # Number of repairmen
