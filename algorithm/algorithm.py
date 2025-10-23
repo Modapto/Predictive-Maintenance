440b713151b07cf651919188d969ace1838cbdf5
Mja1110 <truongle11102000@gmail.com>
2025-10-23 14:09:48 +0200

    update

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -175,8 +175,8 @@ logger = logging.getLogger(__name__)
 # w_max = parameters.w_max       
 
 # GENOME_LENGTH = 17
-POPULATION_SIZE = 100
-GENERATIONS = 1000
+POPULATION_SIZE = 10000
+GENERATIONS = 100000
 p_c_min = 0.6
 p_c_max = 0.9
 p_m_min = 0.01
